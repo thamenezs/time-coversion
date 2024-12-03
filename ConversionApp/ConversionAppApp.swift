983ed8f8a843032217bc44bb5b20e4d1414dd321
Thais Menezes <thais.mas@hotmail.com>
2024-12-03 14:41:23 -0300

    Initial Commit

---
+++ ConversionApp/ConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionAppApp.swift
+//  ConversionApp
+//
+//  Created by Thais Souza on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
