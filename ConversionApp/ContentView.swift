02ce97783b4866f00f4df9af94df8f807e8c97de
Thais Menezes <thais.mas@hotmail.com>
2024-12-03 16:49:26 -0300

    first commit

--- ConversionApp/ContentView.swift
+++ ConversionApp/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var input = "Seconds"
+    @State private var output = "Minutes"
+    @State private var inputNumber = 0.0
+    let conversionOptions = ["Seconds", "Minutes", "Hours", "Days"]
+    
+    var conversion: Double {
+        var inputInSeconds = 0.0
+        var conversed = 0.0
+        
+        switch input {
+        case "Minutes":
+            inputInSeconds = inputNumber * 60
+            
+        case "Hours":
+            inputInSeconds = inputNumber * 3600
+            
+        case "Days":
+            inputInSeconds = inputNumber * 86400
+            
+        case "Seconds":
+            inputInSeconds = inputNumber
+            
+        default: break
+            
         }
-        .padding()
+        
+        if output == "Minutes" {
+            conversed = inputInSeconds / 60
+        } else if output == "Hours" {
+            conversed = inputInSeconds / 3600
+        } else if output == "Days" {
+            conversed = inputInSeconds / 86400
+        } else {
+            conversed = inputInSeconds
+        }
+        
+        return conversed
+    }
+    var body: some View {
+        NavigationStack{
+            Form {
+                Section ("Transform"){
+                    Picker("To be conversed", selection: $input) {
+                        ForEach(conversionOptions, id: \.self){
+                            Text($0)
+                        }
+                        
+                    }
+                    .pickerStyle(.segmented)
+                    
+                }
+                
+                Section ("into"){
+                    Picker("Into", selection: $output) {
+                        ForEach(conversionOptions, id: \.self){
+                            Text($0)
+                        }
+                        
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section{
+                    TextField("To be conversed", value: $inputNumber, format: .number)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section{
+                    Text(conversion, format: .number)
+                }
+                
+                .navigationTitle("Conversion App")
+            }
+        }
     }
 }
 
